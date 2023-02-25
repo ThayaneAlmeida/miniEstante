597b44003f6dafd413719e6134d50357dea3fa9c
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-02-25 20:56:19 -0300

    Controller e Repository inicio

--- miniEstante/src/miniEstante/controller/EmprestimoController.java
+++ miniEstante/src/miniEstante/controller/EmprestimoController.java
@@ -1,5 +1,24 @@
 package miniEstante.controller;
 
-public class EmprestimoController {
+import miniEstante.repository.EmprestimoRepository;
 
+public class EmprestimoController implements EmprestimoRepository {
+
+	private String dataAluguel;
+	private String dataDevolucao;
+	private int idCliente;
+	private int codigoLivro;
+	@Override
+	
+	public void procuraLivroAlugado(int numero) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void listarLivrosAlugados() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
