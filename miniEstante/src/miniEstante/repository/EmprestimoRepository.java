4349706eb5176ef5205e9aea056107c7583f89ff
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-02-25 20:47:41 -0300

    Inicio de Repository e Controller

--- miniEstante/src/miniEstante/repository/EmprestimoRepository.java
+++ miniEstante/src/miniEstante/repository/EmprestimoRepository.java
@@ -1,5 +1,7 @@
 package miniEstante.repository;
 
-public class EmprestimoRepository {
+public interface EmprestimoRepository {
 
+	public void procuraLivroAlugado(int numero);
+	public void listarLivrosAlugados();
 }
