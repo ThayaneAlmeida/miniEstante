c697efbae70e7b212e3bc0a37d64bb89f0441ee3
Gustavo Santiago <gustavosantiago81@yahoo.com.br>
2023-02-26 13:26:39 -0300

    Emprestimo - Controller E Repository finalizados, primeiro teste com a model

--- miniEstante/src/miniEstante/controller/EmprestimoController.java
+++ miniEstante/src/miniEstante/controller/EmprestimoController.java
@@ -1,24 +1,58 @@
 package miniEstante.controller;
 
+import java.util.ArrayList;
+
+import miniEstante.model.Emprestimo;
 import miniEstante.repository.EmprestimoRepository;
 
 public class EmprestimoController implements EmprestimoRepository {
 
-	private String dataAluguel;
-	private String dataDevolucao;
-	private int idCliente;
-	private int codigoLivro;
+	private ArrayList<Emprestimo> listaEmprestimo = new ArrayList<Emprestimo>();
+
 	@Override
-	
 	public void procuraLivroAlugado(int numero) {
-		// TODO Auto-generated method stub
-		
+		// Pegar Id do livro
+
 	}
+
 	@Override
 	public void listarLivrosAlugados() {
-		// TODO Auto-generated method stub
+
+		for (var emprestimo : listaEmprestimo)
+			emprestimo.visualizar();
+
+	}
+
+	@Override
+	public void registrarEmprestimo(Emprestimo emprestimo) {
+		listaEmprestimo.add(emprestimo);
+		System.out.println("Empréstimo Registrado!");
+	}
+
+	@Override
+	public void renovarEmprestimo(Emprestimo emprestimo) {
+	 var buscaEmprestimo = buscarAluguelNaCollection(emprestimo.getIdLivro)
 		
 	}
-	
-	
+
+	@Override
+	public void deletarEmprestimo(int idLivro) {
+		var emprestimo = buscarAluguelNaCollection(idLivro);
+		
+		if(idLivro != null) {
+			if(listaEmprestimo.remove(idLivro)== true)
+				System.out.println("O Empréstimo " + idLivro + " foi retirado da lista!");
+		}else
+			System.out.println("O Empréstimo " + idLivro + " não foi encontrado!");
+	}
+
+
+	public Emprestimo buscarAluguelNaCollection(int idLivro) {
+		for (var emprestimo : listaEmprestimo) {
+			if (emprestimo.getIdLivro() == idLivro)
+				return emprestimo;
+		}
+
+		return null;
+	}
 }
--- miniEstante/src/miniEstante/repository/EmprestimoRepository.java
+++ miniEstante/src/miniEstante/repository/EmprestimoRepository.java
@@ -4,4 +4,8 @@ public interface EmprestimoRepository {
 
 	public void procuraLivroAlugado(int numero);
 	public void listarLivrosAlugados();
+	
+	public void registrarEmprestimo();
+	public void renovarEmprestimo();
+	public void deletarEmprestimo();
 }
